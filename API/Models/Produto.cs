0dbd9e62be1513597a391ad2d83d2a94a3b7a3d4
AnaOliari <carolina97627687@gmail.com>
2024-03-25 23:28:06 -0300

    segunda aula montando essa api

---
+++ API/Models/Produto.cs
@@ -0,0 +1,29 @@
+﻿namespace API.Models;
+
+public class Produto
+{
+  public Produto(string nome, string descricao, double preco)
+  {
+    Nome = nome;
+    Descricao = descricao;
+    Preco = preco;
+  }
+  //Aributo ou propriedade - nome e descrição
+  public string? Nome { get; set;}
+  public string? Descricao { get; set;}
+
+  //prop+tab
+  public double Preco { get; set; }
+
+
+  // private string nome;
+  // private string descrição;
+
+  // public string getNome(){
+  //   return this.nome;
+  // }
+
+  // public void setNome(string nome){
+  //   this.nome = nome;
+  // }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,17 +1,52 @@
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-List<Produto> produtos = new List<Produto>()
+List<Produto> produtos = 
+[
+  new Produto("Celular", "Android", 400.00),
+  new Produto("Televisao", "Sansung", 500.00),
+  new Produto("Celular", "Iphone", 1.500)
+];
+//End Points = Funcionalidades - JSON
+//Post: http://localhost:5090/api/produto/cadastrar
+//cadastrar um produto na lista 
+//a)atraves das informações na url
+//b) atraves das informações no corpo da requisição
+//Realizar as operações alterações e remoção da lista 
+app.MapPost("/api/produto/cadastrar", () => "Hello World!");
 
+//GET: http://localhost:5090/api/produto/listar
+app.MapGet("/api/produto/listar", () => produtos);
+
+//GET: http://localhost:5090/api/produto/buscar/{nomedoproduto}
+app.MapGet("/api/produto/buscar/{nome}", ([FromRoute] string nome) => 
 {
-  new Produto("Celular", "Android"),
-  new Produto("Televisao", "Sansung"),
-  new Produto("Celular", "Iphone")
-};
+  //Enpoint com varias linhas de codigo
+  // Console.WriteLine(nome);
+  for (int i = 0; i < produtos.Count; i++)
+  {
+    if (produtos[i].Nome == nome)
+    {
+      return Results.Ok(produtos[i]);
+    }
+  }
+  return Results.NotFound();
 
-app.MapGet("/", () => "Hello World!");
-app.MapPost("/api/produto", () => produtos);
+
+});
+
+
 // cadastrar um produto na lista produto 
 app.Run();
 
-public record Produto(string Nome, string Descricao);
+// Produto produto = new Produto;
+// // produto.setNome("Nome do produto");
+// // Console.WriteLine(produto.getNome);
+
+// produto.Nome = "Nome do produto";
+// Console.WriteLine(produto.Nome);
+
+
